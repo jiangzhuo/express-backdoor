94f9096281d17c852cc8c175b54229afabab2235
jiangzhuo <glmmmmmm@163.com>
2017-06-05 14:15:24 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # express-backdoor
+
+如何给express项目明目张胆地添加后门。
+
+示例代码在 ``demo/example.js`` 中
+使用 demo/index.html 用来访问后门
+index.html 来自 [**xterm.js**](https://github.com/sourcelair/xterm.js) 的demo
--- demo/example.js
+++ demo/example.js
@@ -1,8 +1,7 @@
 var express = require('express')
-var backdoor = require('./index')
+var backdoor = require('../index')
 
 var app = express();
 
-// Serve URLs like /ftp/thing as public/ftp/thing
 backdoor(app, {path: '/terminals'});
 app.listen(3000);
